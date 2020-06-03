5c1419c9e5afdd08d09bd597958a566ae12f19fa
Julio Olivares <julioolivares90@hotmail.com>
2020-06-02 20:27:06 -0600

    add dbcontext

--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SocialMedia.Infrastructure.Data;
 
 namespace SocialMedia.Api
 {
@@ -27,7 +29,10 @@ namespace SocialMedia.Api
         {
             services.AddControllers();
 
-            
+            services.AddDbContext<SocialMediaContext>(options => {
+
+                options.UseSqlServer(Configuration.GetConnectionString("SocialMedia"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ SocialMedia.Infrastructure/Repositories/PublicacionRepository.cs
@@ -0,0 +1,29 @@
+﻿using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SocialMedia.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PublicacionRepository : IPublicacionRepository
+    {
+        private readonly SocialMediaContext _Context;
+        public PublicacionRepository(SocialMediaContext context)
+        {
+            _Context = context;
+        }
+
+        
+
+        public async Task<IEnumerable<Publicacion>> GetPublicaciones()
+        {
+            var publicaciones = await  _Context.Publicacion.ToListAsync();
+
+            return publicaciones;
+        }
+    }
+}
