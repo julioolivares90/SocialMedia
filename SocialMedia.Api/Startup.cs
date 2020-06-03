bd906a07659489814d55361c574b6ff300daa6f9
Julio Olivares <julioolivares90@hotmail.com>
2020-06-02 18:34:21 -0600

    cambios

--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -26,6 +26,8 @@ namespace SocialMedia.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
